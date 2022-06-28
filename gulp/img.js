39735a1040a39a673365e4bd427156a42e688cea
Andrey <90615010+shtabnov@users.noreply.github.com>
2022-06-28 19:57:04 +0500

    features section

--- gulp/img.js
+++ gulp/img.js
@@ -3,7 +3,7 @@ const imagemin = require('gulp-imagemin');
 
 
 module.exports = function minImg () {
-    return gulp.src('src/images/*')
+    return gulp.src('src/images/**/*')
       .pipe(imagemin())
       .pipe(gulp.dest('build/images'))
   }
