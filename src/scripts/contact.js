21b18b983ffbc5c56c4065256cce1bc965b325ce
shtabnov <90615010+shtabnov@users.noreply.github.com>
2022-07-08 15:51:10 +0500

    update contact map

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,21 +1,33 @@
 ymaps.ready(init);
 
+let center = [];
+let width = document.documentElement.clientWidth;
+if (width < 769) {
+    center = [57.97826988901288, 56.103487233862566];
+} else {
+    center = [57.97827456461129, 56.104658134476004]
+}
+
 function init(){
     var map = new ymaps.Map("map", {
-        center: [57.97771683602552, 56.10550914007951],
+        center: center,
         zoom: 18
-
     });
 
     var placemark = new ymaps.Placemark([57.97826988901288, 56.103487233862566], {
         balloonContentHeader: 'Sculptura',
         balloonContentBody: 'д. Кондратово ул. Карла Маркса, 8А',
         balloonContentFooter: '8 963-881-62-67',
-        hintContent: 'Зимние происшествия'
+        hintContent: 'Забота о себе'
+    },
+    {
+        iconLayout: 'default#image',
+        iconImageHref: '../images/icon/location.svg',
+        iconImageSize: [100, 100],
+        iconImageOffset: [-40, -100]
     });
 
     map.geoObjects.add(placemark);
-    placemark.balloon.open();
 
 
     map.controls.remove('geolocationControl'); // удаляем геолокацию
