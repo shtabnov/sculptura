e4df3a19f2a795b0b0270e2dffcf4abf1133a79a
shtabnov <90615010+shtabnov@users.noreply.github.com>
2022-07-11 16:50:46 +0500

    reviews block

---
+++ src/scripts/reviews.js
@@ -0,0 +1,17 @@
+new Splide( '#reviews_slider', {
+    perPage: 2,
+    perMove: 1,
+    type: 'loop',
+
+    breakpoints: {
+		1025: {
+            fixedHeight: '442px',
+		},
+
+        769: {
+            perPage: 1,
+            fixedWidth: 'calc(100% - 0px)',
+            fixedHeight: '340px',
+		},
+    }
+} ).mount();
