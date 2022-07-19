636e272336f0b39edd8304a57c9cdbfffa35fb44
shtabnov <90615010+shtabnov@users.noreply.github.com>
2022-07-19 15:53:55 +0500

    up

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,17 +1,17 @@
 new Splide( '#reviews_slider', {
     perPage: 2,
-    perMove: 1,
+    perMove: 2,
     type: 'loop',
 
     breakpoints: {
-		1025: {
-            fixedHeight: '442px',
-		},
+      1025: {
+              fixedHeight: '442px',
+      },
 
-        769: {
-            perPage: 1,
-            fixedWidth: 'calc(100% - 0px)',
-            fixedHeight: '340px',
-		},
+      769: {
+          perPage: 1,
+          fixedWidth: 'calc(100% - 0px)',
+          fixedHeight: '340px',
+      },
     }
 } ).mount();
