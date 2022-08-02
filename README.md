6ca1f4793bdde466ab7fd292987a33473527170c
Andrey <90615010+shtabnov@users.noreply.github.com>
2022-08-02 11:24:47 +0500

    build

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# gulp_tamplate
+# верстка по самодельному макету
 
-my gulp build for website layout
+Ссылка на макет [Figma](https://www.figma.com/file/BXImfaWAExK3IAlgjNIsAx/sculptura?node-id=0%3A1).
+
+Использвалось
+- Pug
+- Sass
+- Gulp
+
---
+++ build/scripts/main.js
@@ -0,0 +1,73 @@
+"use strict";
+
+var menuBtn = document.getElementById('menu__toggle');
+var navList = document.querySelector('.nav__list');
+menuBtn.addEventListener('click', function () {
+  navList.classList.toggle('nav__list_active');
+  console.log(navList);
+});
+"use strict";
+
+var slides = document.querySelectorAll('.hero__image');
+var count = slides.length;
+
+function slide() {
+  setInterval(function () {
+    removeActiv();
+    count--;
+    count < 0 ? count = slides.length - 1 : count;
+    addActive(count);
+  }, 5000);
+}
+
+function removeActiv() {
+  slides.forEach(function (e) {
+    return e.classList.remove('hero__image_active');
+  });
+}
+
+function addActive(n) {
+  slides[n].classList.add('hero__image_active');
+}
+
+slide();
+"use strict";
+
+new Splide('#sale_slider', {
+  type: 'loop',
+  perPage: 1
+}).mount();
+"use strict";
+
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+var anchors = document.querySelectorAll('a[href*="#"]');
+
+var _iterator = _createForOfIteratorHelper(anchors),
+    _step;
+
+try {
+  var _loop = function _loop() {
+    var anchor = _step.value;
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      var blockID = anchor.getAttribute('href').substr(1);
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  };
+
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    _loop();
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
