0ce71292756bc2c5c1a62bf066beaeaa75a8aa49
shtabnov <90615010+shtabnov@users.noreply.github.com>
2023-12-12 11:10:03 +0500

    +

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -174,7 +174,7 @@ try {
       var blockID = anchor.getAttribute('href').substr(1);
       document.getElementById(blockID).scrollIntoView({
         behavior: 'smooth',
-        block: 'start'
+        block: 'nearest'
       });
     });
   };
